281e93b288dce78cb010bf415a4d9938cd247a5b
Sohailkhan1246 <162411156+Sohailkhan1246@users.noreply.github.com>
2025-06-26 16:37:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -112,6 +112,6 @@ app.post('/edit-task/:id', checkAuth, async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
